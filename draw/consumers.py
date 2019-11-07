287cc5c3f568cd8a991befd73c6fb8cac9e6002f
unknown <colorsolid@gmail.com>
2019-11-06 18:58:54 -0800

    initial

---
+++ draw/consumers.py
@@ -0,0 +1,37 @@
+from   channels.generic.websocket import AsyncWebsocketConsumer
+import channels.layers
+import json
+import os
+
+
+class DrawConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        print(self.scope['user'])
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'draw_' + self.room_name
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+
+    # message from client, data['type'] calls named function
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            data
+        )
+
+
+    # message to client, called data['type'] in receive()
+    async def draw(self, data):
+        await self.send(text_data=json.dumps(data))
