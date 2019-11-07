8474a343a93388e960da57a9e7de58551b2453fb
colorsolid <colorsolid@gmail.com>
2019-11-07 00:02:10 -0800

    null

--- home/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    context = {}
-    return render(request, 'home/home.html', context)
--- static/js/draw/rooms.js
+++
@@ -1,9 +0,0 @@
-let input = document.getElementById('room-input');
-let btn = document.getElementById('room-submit');
-
-btn.onclick = function() {
-  let room_name = input.value;
-  if (input.value.length) {
-    window.location.pathname = '/draw/' + room_name + '/';
-  }
-}
