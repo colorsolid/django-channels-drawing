aeaa0d5b90041f1f5b168340fd0786e09648950f
colorsolid <colorsolid@gmail.com>
2019-11-07 22:14:29 -0800

    fixed asgi.py

--- foli/asgi.py
+++ foli/asgi.py
@@ -1,5 +1,8 @@
 import os
-import channels.asgi
+import django
+from channels.routing import get_default_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foli.settings')
-channel_layer = channels.asgi.get_channel_layer()
+django.setup()
+application = get_default_application()
