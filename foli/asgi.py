90580b0e187127e54f3bf9ee4ca795fe9089367f
colorsolid <colorsolid@gmail.com>
2019-11-07 22:44:42 -0800

    updated channel_layers host

--- foli/asgi.py
+++ foli/asgi.py
@@ -1,7 +1,6 @@
 import os
 import django
 from channels.routing import get_default_application
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foli.settings')
 django.setup()
