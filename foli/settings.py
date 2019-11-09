e1ae966feff3b43e11fbc23240bfe6307eaa4858
unknown <colorsolid@gmail.com>
2019-11-09 11:55:23 -0800

    whitenoise setup

--- foli/settings.py
+++ foli/settings.py
@@ -153,5 +153,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
